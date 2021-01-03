9e7f02f2d43f496688e804e4de554ceaada1cec1
Flavio D'Asaro <flavio.dasaro@linksmt.it>
2021-01-03 21:33:49 +0100

    fix(FE/all): aggiunta gestione disabled al componente Button

--- FE/src/ui/common/components/form/button/Button.jsx
+++ FE/src/ui/common/components/form/button/Button.jsx
@@ -1,16 +1,17 @@
 import { withNamespaces } from "react-i18next";
 import "./Button.scss";
 
-const ButtonComponent = ({ onClick, type, labelKey, theme, t }) => {
+const ButtonComponent = ({ onClick, type, labelKey, disabled, theme, t }) => {
   return (
-    <button type={type} onClick={onClick} className={theme + " common"}>
+    <button type={type} onClick={onClick} className={theme + " common"} disabled={disabled}>
       {t(labelKey)}
     </button>
   );
 };
 
 ButtonComponent.defaultProps = {
-    theme:'primary'
+    theme:'primary',
+    disabled:false
 }
 
 export const Button = withNamespaces()(ButtonComponent);
