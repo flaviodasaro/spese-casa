c4092a2e1e4f26bbaca9c969cf95b78faf1ffd10
Flavio D'Asaro <flavio.dasaro@linksmt.it>
2021-03-09 10:52:27 +0100

    fix(BE/test): test read env vars

--- BE/src/main/java/com/app/spesecasa/controller/TestController.java
+++ BE/src/main/java/com/app/spesecasa/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.app.spesecasa.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	@Value("${custom.hostname}")
+	private String testDbName;
+
 	@GetMapping("/")
 	String hello() {
-		return "This tutorial is the best. All hail the great Kristijan.";
+		return "DB name: " + testDbName;
 	}
 
 
